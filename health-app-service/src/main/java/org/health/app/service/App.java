ab120abfe361f39388bc4932839f712fcc98ece6
ramakrishna <ramakrishna.snj@gmail.com>
2017-02-10 09:54:55 -0600

    first comment

---
+++ health-app-service/src/main/java/org/health/app/service/App.java
@@ -0,0 +1,13 @@
+package org.health.app.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
